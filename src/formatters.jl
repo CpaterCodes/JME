5751feffa75b6f2e8264d96433ff924afdc3059a
Charlie Paterson <Charlieed.Paterson@googlemail.com>
2023-05-30 19:30:13 +0100

    Feat: HTML linebreaker can handle tag and non-tag content. May refactor later for greater legibility; for now wanted to avoid nesting replace functions in a way which obscures the sequence of steps taken

--- src/formatters.jl
+++ src/formatters.jl
@@ -1,5 +1,14 @@
 
-with_linebreaks = (html::String) -> replace(html, ">" => ">\n")
+function with_linebreaks(html::String)
+	return html |> html -> replace(
+		html, 
+		"<" => "\n<", 
+		">" => ">\n"
+	) |> html -> replace(
+			html, 
+			"\n\n" => "\n"
+	) |> html -> html[2:end]
+end
 
 function with_indents(html::String)
 
--- test/test_linebreaks.jl
+++ test/test_linebreaks.jl
@@ -2,9 +2,14 @@ using Test
 include("../src/formatters.jl")
 
 @testset "Will add linebreaks to condensed html" begin
-	before = "<ul><li></li><li></li></ul>"
-	after = "<ul>\n<li>\n</li>\n<li>\n</li>\n</ul>\n"
+	before = "<ul><li>Foo</li><li>Bar</li></ul>"
+	after = "<ul>\n<li>\nFoo\n</li>\n<li>\nBar\n</li>\n</ul>\n"
 	
+	@test with_linebreaks(before) == after
+	
+	before = "<main><div>Hello World!</div></main>"
+	after = "<main>\n<div>\nHello World!\n</div>\n</main>\n"
+
 	@test with_linebreaks(before) == after
 end
 
