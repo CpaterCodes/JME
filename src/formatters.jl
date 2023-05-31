2bd9f7e00cae28953075730f0b5be423bf91f025
Charlie Paterson <Charlieed.Paterson@googlemail.com>
2023-05-31 17:26:45 +0100

    Feat: can now indent a section of html, assuming it already has linebreaks.

--- src/formatters.jl
+++ src/formatters.jl
@@ -11,6 +11,26 @@ function with_linebreaks(html::String)
 end
 
 function with_indents(html::String)
+	rows::Vector{String} = split(html, "\n")
+	depth::Integer = 0
+	for index = 1:length(rows)
 
+		row = rows[index]
+		if occursin("</", row) && depth > 0 
+			depth -= 1
+			rows[index] = repeat("\t", depth) * row
+		
+		elseif occursin("/>", row)
+			rows[index] = repeat("\t", depth) * row
+
+		elseif occursin(">", row)
+			rows[index] = repeat("\t", depth) * row
+			depth += 1
+
+		else
+			rows[index] = repeat("\t", depth) * row
+		end
+	end
+	return join(rows, "\n")
 end
 
--- test/test_indents.jl
+++ test/test_indents.jl
@@ -1,2 +1,13 @@
 using Test
 include("../src/formatters.jl")
+
+@testset "Will indent linebroken html appropriately" begin
+	before = "<main>\n<div>\nHello World!\n</div>\n</main>\n"
+	after = """<main>
+		<div>
+			Hello World!
+		</div>
+	</main>
+	"""
+	@test with_indents(before) == after
+end
